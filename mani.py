1e69edefc0825f88308ae21daf2943530d5760cf
mralimov <mralimov89@gmail.com>
2021-03-29 16:56:25 -0400

    importing and implementing Snake class

--- mani.py
+++ mani.py
@@ -1,23 +1,16 @@
 from turtle import Turtle, Screen
-
+import time
+from snake import Snake
 screen = Screen()
 screen.setup(width=600, height=600)
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-all_snakes = []
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-for position in starting_position:
-    snake = Turtle(shape="square")
-    snake.color("white")
-    snake.penup()
-    snake.goto(position)
-    all_snakes.append(snake)
+snake = Snake()
 
-
-#snake.turtlesize(1, 3)
+# snake.turtlesize(1, 3)
 
 game_is_on = True
 
@@ -25,9 +18,5 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for snake_i in range(len(all_snakes)-1, 0, -1):
-        new_x = all_snakes[snake_i - 1].xcor()
-        new_y = all_snakes[snake_i - 1].ycor()
-        all_snakes[snake_i].goto(new_x, new_y)
-    all_snakes[0].forward(20)
+   snake.move()
 screen.exitonclick()
