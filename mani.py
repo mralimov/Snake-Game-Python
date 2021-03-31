974177337763d4aff74e703d3013b448e4f0a185
mralimov <mralimov89@gmail.com>
2021-03-31 14:36:14 -0400

    Event listener on screen onkey

--- mani.py
+++ mani.py
@@ -14,6 +14,12 @@ snake = Snake()
 
 game_is_on = True
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
