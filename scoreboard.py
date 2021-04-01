b136d7dca2af3681c19eafafbdff3d3afbaedd5e
mralimov <mralimov89@gmail.com>
2021-03-31 23:39:25 -0400

    refactor scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 12, "bold")
 
 
 class Scoreboard(Turtle):
@@ -8,11 +10,12 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center",
-                   font=("Arial", 12, "bold"))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
         def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center",
-                   font=("Arial", 12, "bold"))
+        self.update_scoreboard()
