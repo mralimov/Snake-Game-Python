dacd5817aa38f418837c37a906bd5526d36bcb99
mralimov <mralimov89@gmail.com>
2021-03-31 23:35:04 -0400

    Score board class created

---
+++ scoreboard.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align="center",
+                   font=("Arial", 12, "bold"))
