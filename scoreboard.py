fc8cd62b0a59c37be0851016880a8abfb48d11d8
mralimov <mralimov89@gmail.com>
2021-04-01 10:21:37 -0400

    Game over function

--- scoreboard.py
+++ scoreboard.py
@@ -19,3 +19,7 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER: {self.score}", align=ALIGNMENT, font=FONT)
