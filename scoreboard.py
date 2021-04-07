02d197b4f6e53f3f87bb12ca0111b3340351f9ba
mralimov <mralimov89@gmail.com>
2021-04-07 09:49:56 -0400

    set the highest score method

--- scoreboard.py
+++ scoreboard.py
@@ -7,13 +7,16 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highest_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(
+            f"Score: {self.score} Highest Score: {self.highest_score}", align=ALIGNMENT, font=FONT)
 
         def increase_score(self):
         self.score += 1
