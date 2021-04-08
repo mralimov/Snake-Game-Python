c05d96e0b3d0fbdf9a9ee37948348a1bbb698330
mralimov <mralimov89@gmail.com>
2021-04-07 21:41:07 -0400

    create highest score data and read it

--- scoreboard.py
+++ scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highest_score = 0
+        with open("data.txt") as data:
+            self.highest_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
